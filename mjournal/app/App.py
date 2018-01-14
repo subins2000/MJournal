373d52074a03add458568a540ea883dfc4093641
Subin Siby <subins2000@gmail.com>
2018-01-14 11:53:22 +0530

    Addd pyqt app files

---
+++ mjournal/app/App.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+import sys
+
+from PyQt5.QtCore import QTranslator, QLocale, QLibraryInfo
+from PyQt5.QtWidgets import QApplication, QWidget
+
+from mjournal.app.Home import Home
+
+
+class App:
+
+    app = None
+
+    def run(self):
+        self.app = QApplication(sys.argv)
+
+        qtTranslator = QTranslator()
+        qtTranslator.load('mjournal_' + QLocale.system().name(),
+                          QLibraryInfo.location(QLibraryInfo.TranslationsPath))
+        self.app.installTranslator(qtTranslator)
+
+        ex = Home()
+
+        sys.exit(self.app.exec_())
---
+++ mjournal/app/Home.py
@@ -0,0 +1,35 @@
+from PyQt5.QtCore import Qt
+from PyQt5.QtWebEngineWidgets import *
+from PyQt5.QtWidgets import qApp, QMainWindow, QStyle
+
+from mjournal.app.ui import Ui_Home
+
+
+class Home(QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+        self.initMenubar()
+
+    def initUI(self):
+        self.ui = Ui_Home()
+        self.ui.setupUi(self)
+
+        self.browserView = QWebEngineView()
+        self.centralWidget.add(browser)
+
+        self.setGeometry(
+            QStyle.alignedRect(
+                Qt.LeftToRight,
+                Qt.AlignCenter,
+                self.size(),
+                qApp.desktop().availableGeometry()
+            )
+        )
+
+        self.show()
+
+    def initMenubar(self):
+        self.ui.quitAction.triggered.connect(qApp.quit)
