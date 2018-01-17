c5f2d6a35382a99d851c8e377b0712db526f81af
Subin Siby <subins2000@gmail.com>
2018-01-17 19:36:19 +0530

    Serve static files

--- mjournal/server/server.py
+++ mjournal/server/server.py
@@ -4,13 +4,30 @@ from flask import Flask
 from flask import render_template
 
 
-template_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
+server_loc = os.path.dirname(os.path.abspath(__file__))
 
-server = Flask('mjournal', template_folder=template_dir)
+template_loc = os.path.join(server_loc, 'templates')
+server = Flask('mjournal', template_folder=template_loc)
 
 @server.route("/")
 def index():
     return render_template('index.html')
+
+@server.route('/js/<path:path>')
+def send_js(path):
+    global server_loc
+
+    js_loc = os.path.join(server_loc, 'static', 'js')
+
+    return send_from_directory(js_loc, path)
+
+@server.route('/css/<path:path>')
+def send_css(path):
+    global server_loc
+
+    css_loc = os.path.join(server_loc, 'static', 'css')
+
+    return send_from_directory(css_loc, path)
 
 def run_server():
     global server
