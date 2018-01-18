85b8be145eee3d609a844d9165cb70293f0e827b
Subin Siby <subins2000@gmail.com>
2018-01-18 23:28:05 +0530

    Pass vuejs templates as string to index.html template file

--- mjournal/server/server.py
+++ mjournal/server/server.py
@@ -9,9 +9,23 @@ server_loc = os.path.dirname(os.path.abspath(__file__))
 template_loc = os.path.join(server_loc, 'templates')
 server = Flask('mjournal', template_folder=template_loc)
 
+def get_template_content(filename):
+    global server_loc
+
+    path = os.path.join(server_loc, 'templates', filename)
+
+    with open(path, 'r') as myfile:
+        data = myfile.read().replace('\n', '')
+
+    return data
+
 @server.route("/")
 def index():
-    return render_template('index.html')
+    return render_template('index.html',
+        home_template=get_template_content('home.html'),
+        write_template=get_template_content('write.html'),
+        settings_template=get_template_content('settings.html')
+    )
 
 @server.route('/js/<path:path>')
 def send_js(path):
