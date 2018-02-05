38dadac237bff6fd9f64a6c578a7aa8b707db016
Subin Siby <subins2000@gmail.com>
2018-02-05 22:51:15 +0530

    Added a root_loc var

--- mjournal/MJournal.py
+++ mjournal/MJournal.py
@@ -8,8 +8,16 @@ class MJournal:
 
     app = None
 
+    root_loc = None
+
+    '''
+    root_loc - Root location of MJournal
+    '''
+    def __init__(self, root_loc = None):
+        self.root_loc = root_loc
+
     def run_server(self):
-        _thread.start_new_thread(run_server, ())
+        _thread.start_new_thread(run_server, (self.root_loc,))
 
     def run_app(self):
         self.app = App()
