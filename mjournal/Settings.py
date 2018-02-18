a529fc8632057d65a8ce026958bcdb980073a19b
Subin Siby <subins2000@gmail.com>
2018-02-18 23:48:59 +0530

    Receive and store root_loc as property

--- mjournal/Settings.py
+++ mjournal/Settings.py
@@ -5,6 +5,11 @@ from PyQt5.QtCore import QSettings
 
 class Settings:
 
+    root_loc = None
+
+    def __init__(self, root_loc):
+        self.root_loc = root_loc
+
     def get_value(self, key):
         settings = QSettings('mjournal', 'mjournal')
         return settings.value(key)
@@ -15,9 +20,9 @@ class Settings:
         if save_loc:
             return save_loc
         else:
-            data_path = os.path.join(root_loc, 'data')
+            data_path = os.path.join(self.root_loc, 'data')
 
             if not os.path.exists(data_path):
                 os.makedirs(data_path)
 
-            return os.path.join(root_loc, 'data', 'entries')
+            return os.path.join(self.root_loc, 'data', 'entries')
